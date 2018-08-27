c9a3125e47326e262c5662725fec3de447ea18c8
Pharap <2933055+Pharap@users.noreply.github.com>
2018-08-28 00:25:55 +0100

    Improve variable locality and constness (#10)
    Moves variables closer to where they're used and marks unchanging variables as const.

--- Arduboy2/SpritesB.cpp
+++ Arduboy2/SpritesB.cpp
@@ -36,18 +36,16 @@ void SpritesB::drawPlusMask(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t
 //common functions
 void SpritesB::draw(int16_t x, int16_t y, const uint8_t *bitmap, uint8_t frame, const uint8_t *mask, uint8_t sprite_frame, uint8_t drawMode)
 {
-	unsigned int frame_offset;
-
 	if(bitmap == NULL)
 		return;
 
-	uint8_t width = pgm_read_byte(bitmap);
+	const uint8_t width = pgm_read_byte(bitmap);
 	++bitmap;
-	uint8_t height = pgm_read_byte(bitmap);
+	const uint8_t height = pgm_read_byte(bitmap);
 	++bitmap;
 	if(frame > 0 || sprite_frame > 0)
 	{
-		frame_offset = (width * ( height / 8 + ( height % 8 == 0 ? 0 : 1)));
+		unsigned int frame_offset = (width * ( height / 8 + ( height % 8 == 0 ? 0 : 1)));
 		// sprite plus mask uses twice as much space for each frame
 		if(drawMode == SPRITE_PLUS_MASK)
 			frame_offset *= 2;
@@ -75,34 +73,22 @@ void SpritesB::drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, const uin
 
 	// xOffset technically doesn't need to be 16 bit but the math operations
 	// are measurably faster if it is
-	uint16_t xOffset, ofs;
-	int8_t yOffset = y & 7;
-	int8_t sRow = y / 8;
-	uint8_t loop_h, start_h, rendered_width;
-
-	if(y < 0 && yOffset > 0)
-		sRow--;
-
 	// if the left side of the render is offscreen skip those loops
-	if(x < 0)
-		xOffset = abs(x);
-	else
-		xOffset = 0;
+	const uint16_t xOffset = (x < 0) ? abs(x) : 0;
 
 	// if the right side of the render is offscreen skip those loops
-	if(x + w > WIDTH - 1)
-		rendered_width = ((WIDTH - x) - xOffset);
-	else
-		rendered_width = (w - xOffset);
+	const uint8_t rendered_width = (x + w > WIDTH - 1) ? ((WIDTH - x) - xOffset) : (w - xOffset);
+	
+	const int8_t yOffset = y & 7;
+	const int8_t tempSRow = y / 8;
+	int8_t sRow = (y < 0 && yOffset > 0) ? tempSRow - 1 : tempSRow;
 
 	// if the top side of the render is offscreen skip those loops
-	if(sRow < -1)
-		start_h = abs(sRow) - 1;
-	else
-		start_h = 0;
+	const uint8_t start_h = (sRow < -1) ? abs(sRow) - 1 : 0;
 
 	// divide, then round up
-	loop_h = h / 8 + (h % 8 > 0 ? 1 : 0); 
+	const uint8_t rows = h / 8;
+	uint8_t loop_h = ((h % 8) > 0) ? (rows + 1) : rows;
 
 	// if(sRow + loop_h - 1 > (HEIGHT/8)-1)
 	if(sRow + loop_h > (HEIGHT / 8))
@@ -111,48 +97,43 @@ void SpritesB::drawBitmap(int16_t x, int16_t y, const uint8_t *bitmap, const uin
 	// prepare variables for loops later so we can compare with 0
 	// instead of comparing two variables
 	loop_h -= start_h;
-
 	sRow += start_h;
-	ofs = (sRow * WIDTH) + x + xOffset;
+	
+	const uint8_t mul_amt = 1 << yOffset;
 
-	uint8_t mul_amt = 1 << yOffset;
-	uint16_t mask_data;
-	uint16_t bitmap_data;
+	const uint8_t ofs_step = (draw_mode == SPRITE_PLUS_MASK) ? 2 : 1;
+	const uint8_t ofs_stride = (w - rendered_width) * ofs_step;
+	const uint16_t initial_bofs = ((start_h * w) + xOffset) * ofs_step;
 
-	const uint8_t ofs_step = draw_mode == SPRITE_PLUS_MASK ? 2 : 1;
-	const uint8_t ofs_stride = (w - rendered_width)*ofs_step;
-	const uint16_t initial_bofs = ((start_h * w) + xOffset)*ofs_step;
-
-	const uint8_t *bofs = bitmap + initial_bofs;
-	const uint8_t *mask_ofs = !mask ? bitmap : mask;
-	mask_ofs += initial_bofs + ofs_step - 1;
+	uint16_t ofs = (sRow * WIDTH) + x + xOffset;
+	const uint8_t * bofs = bitmap + initial_bofs;
+	const uint8_t * mask_ofs = ((mask == NULL) ? bitmap : mask) + initial_bofs + ofs_step - 1;
 
 	for(uint8_t a = 0; a < loop_h; ++a)
 	{
 		for(uint8_t iCol = 0; iCol < rendered_width; ++iCol)
 		{
-			uint8_t data;
+			uint16_t bitmap_data = pgm_read_byte(bofs) * mul_amt;
+			uint16_t mask_data = ~(pgm_read_byte(mask_ofs) * mul_amt);
 
-			bitmap_data = pgm_read_byte(bofs) * mul_amt;
-			mask_data = ~bitmap_data;
-
-			if(draw_mode == SPRITE_UNMASKED)
-				mask_data = ~(0xFF * mul_amt);
-			else if(draw_mode == SPRITE_IS_MASK_ERASE)
+			if(draw_mode == SPRITE_IS_MASK_ERASE)
+			{
+				mask_data = ~bitmap_data;
 				bitmap_data = 0;
-			else
-				mask_data = ~(pgm_read_byte(mask_ofs) * mul_amt);
+			}
+			else if(draw_mode == SPRITE_UNMASKED)
+				mask_data = ~(0xFF * mul_amt);
 
 			if(sRow >= 0)
 			{
-				data = Arduboy2Base::sBuffer[ofs];
+				uint8_t data = Arduboy2Base::sBuffer[ofs];
 				data &= static_cast<uint8_t>(mask_data);
 				data |= static_cast<uint8_t>(bitmap_data);
 				Arduboy2Base::sBuffer[ofs] = data;
 			}
 			if(yOffset != 0 && sRow < 7)
 			{
-				data = Arduboy2Base::sBuffer[ofs + WIDTH];
+				uint8_t data = Arduboy2Base::sBuffer[ofs + WIDTH];
 				data &= (*(reinterpret_cast<unsigned char *>(&mask_data) + 1));
 				data |= (*(reinterpret_cast<unsigned char *>(&bitmap_data) + 1));
 				Arduboy2Base::sBuffer[ofs + WIDTH] = data;
