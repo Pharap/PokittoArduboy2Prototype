d769ad7856e3c9a3093aa6398371653784295bab
Pharap <2933055+Pharap@users.noreply.github.com>
2018-08-31 12:20:17 +0100

    Disable audio functionality (#17)
    Disables all references to the tone function and the Arduboy audio pins.

--- Arduboy2/Arduboy2Beep.cpp
+++ Arduboy2/Arduboy2Beep.cpp
@@ -4,7 +4,7 @@
  * Classes to generate simple square wave tones on the Arduboy speaker pins.
  */
 
-#include <Arduino.h>
+//#include <Arduino.h>
 #include "Arduboy2Beep.h"
 
 #ifndef AB_DEVKIT
@@ -15,26 +15,32 @@ uint8_t BeepPin1::duration = 0;
 
 void BeepPin1::begin(void)
 {
-	TCCR3A = 0;
+	// TODO: Implement BeepPin1::begin(void)
 
-	// CTC mode. Divide by 8 clock prescale
-	TCCR3B = (bit(WGM32) | bit(CS31));
+	//TCCR3A = 0;
+
+	//// CTC mode. Divide by 8 clock prescale
+	//TCCR3B = (bit(WGM32) | bit(CS31));
 }
 
 void BeepPin1::tone(uint16_t count)
 {
-	tone(count, 0);
+	// TODO: Implement BeepPin1::tone(uint16_t count)
+
+	//tone(count, 0);
 }
 
 void BeepPin1::tone(uint16_t count, uint8_t dur)
 {
 	duration = dur;
 
-	// set toggle on compare mode (which connects the pin)
-	TCCR3A = bit(COM3A0);
+	// TODO: Implement BeepPin1::tone(uint16_t count, uint8_t dur)
 
-	// load the count (16 bits), which determines the frequency
-	OCR3A = count;
+	//// set toggle on compare mode (which connects the pin)
+	//TCCR3A = bit(COM3A0);
+
+	//// load the count (16 bits), which determines the frequency
+	//OCR3A = count;
 }
 
 void BeepPin1::timer(void)
@@ -44,17 +50,21 @@ void BeepPin1::timer(void)
 
 	--duration;
 
-	// set normal mode (which disconnects the pin)
-	if(duration == 0)
-		TCCR3A = 0;
+	// TODO: Implement BeepPin1::timer(void)
+
+	//// set normal mode (which disconnects the pin)
+	//if(duration == 0)
+		//TCCR3A = 0;
 }
 
 void BeepPin1::noTone(void)
 {
 	duration = 0;
 
-	// set normal mode (which disconnects the pin)
-	TCCR3A = 0;
+	// TODO: Implement BeepPin1::noTone(void)
+
+	//// set normal mode (which disconnects the pin)
+	//TCCR3A = 0;
 }
 
 
@@ -64,39 +74,45 @@ uint8_t BeepPin2::duration = 0;
 
 void BeepPin2::begin(void)
 {
-	// normal mode. Disable PWM
-	TCCR4A = 0;
+	// TODO: Implement BeepPin2::begin(void)
 
-	// divide by 128 clock prescale
-	TCCR4B = bit(CS43);
+	//// normal mode. Disable PWM
+	//TCCR4A = 0;
 
-	// normal mode
-	TCCR4D = 0;
+	//// divide by 128 clock prescale
+	//TCCR4B = bit(CS43);
 
-	// toggle pin at count = 0
-	TC4H = 0;
+	//// normal mode
+	//TCCR4D = 0;
 
-	//  "
-	OCR4A = 0;
+	//// toggle pin at count = 0
+	//TC4H = 0;
+
+	////  "
+	//OCR4A = 0;
 }
 
 void BeepPin2::tone(uint16_t count)
 {
-	tone(count, 0);
+	// TODO: Implement BeepPin2::tone(uint16_t count)
+
+	//tone(count, 0);
 }
 
 void BeepPin2::tone(uint16_t count, uint8_t dur)
 {
 	duration = dur;
 
-	// set toggle on compare mode (which connects the pin)
-	TCCR4A = bit(COM4A0);
+	// TODO: Implement BeepPin2::tone(uint16_t count, uint8_t dur)
 
-	// load the count (10 bits),
-	TC4H = highByte(count);
+	//// set toggle on compare mode (which connects the pin)
+	//TCCR4A = bit(COM4A0);
 
-	//  which determines the frequency
-	OCR4C = lowByte(count);
+	//// load the count (10 bits),
+	//TC4H = highByte(count);
+
+	////  which determines the frequency
+	//OCR4C = lowByte(count);
 }
 
 void BeepPin2::timer(void)
@@ -106,23 +122,27 @@ void BeepPin2::timer(void)
 
 	--duration;
 
-	// set normal mode (which disconnects the pin)
-	if(duration == 0)
-		TCCR4A = 0;
+	// TODO: Implement BeepPin2::timer(void)
+
+	//// set normal mode (which disconnects the pin)
+	//if(duration == 0)
+		//TCCR4A = 0;
 }
 
 void BeepPin2::noTone(void)
 {
 	duration = 0;
 
-	// set normal mode (which disconnects the pin)
-	TCCR4A = 0;
+	// TODO: Implement BeepPin2::noTone(void)
+
+	//// set normal mode (which disconnects the pin)
+	//TCCR4A = 0;
 }
 
 
-#else /* AB_DEVKIT */
+#else // AB_DEVKIT
 
-// *** The pins used for the speaker on the DevKit cannot be directly
+// The pins used for the speaker on the DevKit cannot be directly
 // controlled by a timer/counter. The following "dummy" functions will
 // compile and operate properly but no sound will be produced
 
@@ -134,12 +154,14 @@ void BeepPin1::begin(void)
 
 void BeepPin1::tone(uint16_t count)
 {
-	tone(count, 0);
+	// TODO: Implement BeepPin1::tone(uint16_t count)
+
+	//tone(count, 0);
 }
 
 void BeepPin1::tone(uint16_t count, uint8_t dur)
 {
-	// parameter not used
+	// Intentionally unused
 	(void)count;
 
 	duration = dur;
@@ -165,12 +187,14 @@ void BeepPin2::begin(void)
 
 void BeepPin2::tone(uint16_t count)
 {
-	tone(count, 0);
+	// TODO: Implement BeepPin2::tone(uint16_t count)
+
+	//tone(count, 0);
 }
 
 void BeepPin2::tone(uint16_t count, uint8_t dur)
 {
-	// parameter not used
+	// Intentionally unused
 	(void)count;
 
 	duration = dur;
