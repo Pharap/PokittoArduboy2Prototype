f6a6b738fc86d941ad1d522aa1547a643e2af042
Pharap <2933055+Pharap@users.noreply.github.com>
2018-09-04 00:31:30 +0100

    Alter definition of int_farptr_t and uint_farptr_t (#26)
    Makes them at least 32 bits and guaranteed to be large enough to store a pointer.

--- avr-libc/__farptr__.h
+++ avr-libc/__farptr__.h
@@ -5,10 +5,12 @@
 //
 
 #include <cstdint>
+#include <type_traits>
 
 //
 // Non-standard type aliases
 //
 
-using int_farptr_t = std::int32_t;
-using uint_farptr_t = std::uint32_t;
+// Ensure that the types are at least 32 bits in length, and large enough to store a pointer
+using int_farptr_t = typename std::conditional<(sizeof(std::intptr_t) >= sizeof(std::int32_t)), std::intptr_t, std::int32_t>::type;
+using uint_farptr_t = typename std::conditional<(sizeof(std::uintptr_t) >= sizeof(std::uint32_t)), std::uintptr_t, std::uint32_t>::type;
