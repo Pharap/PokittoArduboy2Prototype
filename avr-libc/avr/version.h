59c1e51ac297211a0a3db13081f1679ee09055cf
Pharap <2933055+Pharap@users.noreply.github.com>
2018-09-05 07:33:45 +0100

    Add avr/version.h implementation (#32)
    Implements versioning as v2.0.0

---
+++ avr-libc/avr/version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//
+// Macros
+//
+
+#define __AVR_LIBC_VERSION_STRING__ "2.0.0"
+#define __AVR_LIBC_VERSION__ 20000UL
+#define __AVR_LIBC_DATE_STRING__ "20150208"
+#define __AVR_LIBC_DATE_ 20150208UL 
+#define __AVR_LIBC_MAJOR__ 2
+#define __AVR_LIBC_MINOR__ 0
+#define __AVR_LIBC_REVISION__ 0
