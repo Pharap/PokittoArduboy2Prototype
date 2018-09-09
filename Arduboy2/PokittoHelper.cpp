c974cbf731fb36b1615731b1debd8bc18c41ad26
Pharap <2933055+Pharap@users.noreply.github.com>
2018-09-09 13:30:03 +0100

    Implement paintScreen functions (#54)
    Implements screen rendering by rendering the Arduboy's display buffer onto the Pokitto's display buffer.
    
    * Add drawArduboy2Buffer functions
    * Implement paintScreen functions

--- Arduboy2/PokittoHelper.cpp
+++ Arduboy2/PokittoHelper.cpp
@@ -10,5 +10,46 @@ namespace Pokitto
 	{		
 		static constexpr std::size_t BaseX = (220 / 2) - (Width / 2);
 		static constexpr std::size_t BaseY = (176 / 2) - (Height / 2);
+		
+		void drawArduboy2Buffer(const std::uint8_t * buffer)
+		{
+			for(std::size_t y = 0; y < (Height / 8); ++y)
+			{
+				const std::size_t columnBase = y * 8;
+				
+				for(std::size_t x = 0; x < Width; ++x)
+				{
+					const std::uint8_t column = buffer[(y * Width) + x];
+					
+					for(std::size_t i = 0, c = column; i < 8; ++i, c >>= 1)
+					{
+						const uint8_t bit = static_cast<std::uint8_t>(c & 0x01);
+						Pokitto::Display::drawPixel(BaseX + x, BaseY + columnBase + i, bit);
+					}
+				}
+			}
+		}
+		
+		void drawArduboy2Buffer(std::uint8_t * buffer, bool clear)
+		{		
+			for(std::size_t y = 0; y < (Height / 8); ++y)
+			{
+				const std::size_t columnBase = y * 8;
+				
+				for(std::size_t x = 0; x < Width; ++x)
+				{					
+					const std::uint8_t column = buffer[(y * Width) + x];
+					
+					if(clear)
+						buffer[(y * Width) + x] = 0;
+					
+					for(std::size_t i = 0, c = column; i < 8; ++i, c >>= 1)
+					{
+						const uint8_t bit = static_cast<std::uint8_t>(c & 0x01);
+						Pokitto::Display::drawPixel(BaseX + x, BaseY + columnBase + i, bit);
+					}					
+				}
+			}
+		}
 	};
 }
