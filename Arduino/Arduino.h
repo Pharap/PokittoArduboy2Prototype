63baf75b461d684f7ad0a3c6a48944d259fd0f90
Pharap <2933055+Pharap@users.noreply.github.com>
2018-09-09 06:59:34 +0100

    Remove shiftOut and shiftIn functions

--- Arduino/Arduino.h
+++ Arduino/Arduino.h
@@ -138,8 +138,7 @@ void delayMicroseconds(unsigned int us);
 
 // Pharap: Remove pulseIn and pulseInLong functions
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
-uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+// Pharap: Remove shiftOut and shiftIn functions
 
 void attachInterrupt(uint8_t, void (*)(void), int mode);
 void detachInterrupt(uint8_t);
