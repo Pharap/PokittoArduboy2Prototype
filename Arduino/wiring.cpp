77d9068a05f5b53ef9f343cab20cbe72e5f9b9d5
Pharap <2933055+Pharap@users.noreply.github.com>
2018-09-09 03:44:34 +0100

    Remove timer0 macros

--- Arduino/wiring.cpp
+++ Arduino/wiring.cpp
@@ -24,18 +24,7 @@
 
 // Pharap: Remove "wiring_private.h" include
 
-// the prescaler is set so that timer0 ticks every 64 clock cycles, and the
-// the overflow handler is called every 256 ticks.
-#define MICROSECONDS_PER_TIMER0_OVERFLOW (clockCyclesToMicroseconds(64 * 256))
-
-// the whole number of milliseconds per timer0 overflow
-#define MILLIS_INC (MICROSECONDS_PER_TIMER0_OVERFLOW / 1000)
-
-// the fractional number of milliseconds per timer0 overflow. we shift right
-// by three to fit these numbers into a byte. (for the clock speeds we care
-// about - 8 and 16 MHz - this doesn't lose precision.)
-#define FRACT_INC ((MICROSECONDS_PER_TIMER0_OVERFLOW % 1000) >> 3)
-#define FRACT_MAX (1000 >> 3)
+// Pharap: Remove MICROSECONDS_PER_TIMER0_OVERFLOW, MILLIS_INC, FRACT_INC and FRACT_MAX
 
 // Pharap: Remove timer0_overflow_count, timer0_millis and timer0_fract
 
